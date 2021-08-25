8a88c092ae682bcd91fcbd9399c81a269f106b11
Luke Pavsky <85752004+lukedevj@users.noreply.github.com>
2021-08-25 12:17:16 -0300

    Update __init__.py

--- src/autofeepolicy/utils/__init__.py
+++ src/autofeepolicy/utils/__init__.py
@@ -1,12 +1,3 @@
 from . import config, helpers
 
 __all__ = ['config', 'helpers']
-'''
-0318d4e22cf1f76766bb6c73ce0e83805e9a93b20dac05b357e1c926325c234423:
-    - IF(ILP > 82, 600)
-	- IF(OLP < 25, 1, 5)
-
-avoid:
-	- 73666b6a6c73646a6b6664736a6b6c6b6a6c7364666a6b6c6466736a6b6c6173
-	- cbed5f7f79fc9ba63bd4485bfde70ae795941c9be875d3999ee4741aee293dbd
-'''
